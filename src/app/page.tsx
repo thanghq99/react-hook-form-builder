db96b7fd92a04c3b553aebb77d859edc1d49dd0f
thanghq <haikhauak@gmail.com>
2023-12-21 01:54:56 +0700

    feat: add section infos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,24 @@ import Section from '@/components/Section'
 export default function Home() {
   return (
     <main className="container relative flex h-screen snap-y snap-mandatory flex-col overflow-y-scroll scroll-smooth bg-slate-900">
-      <Section title="Form fields" color="bg-lime-100" />
-      <Section title="Field configuration" color="bg-amber-100" />
-      <Section title="Generated form" color="bg-cyan-100" />
+      <Section
+        title="Form fields"
+        color="bg-lime-100"
+        infos={[
+          'You can start adding fields with Field configuration',
+          'You can save the form config by clicking the Save configuration at the bottom',
+        ]}
+      />
+      <Section
+        title="Field configuration"
+        color="bg-amber-100"
+        infos={['This form allows you to create and update form fields']}
+      />
+      <Section
+        title="Generated form"
+        color="bg-cyan-100"
+        infos={['Form will be automatically generated when you make changes']}
+      />
     </main>
   )
 }
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,15 +1,24 @@
+import { Info } from 'lucide-react'
 import React, { FC, PropsWithChildren } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 interface SectionProps extends PropsWithChildren {
   title: string
   color: string
+  infos?: string[]
 }
 
-const Section: FC<SectionProps> = ({ title, color, children }) => {
+const Section: FC<SectionProps> = ({ title, color, infos, children }) => {
   return (
-    <div className={twMerge('min-h-screen shrink-0 snap-center px-2', color)}>
-      <h1 className="block px-2 py-8 text-2xl">{title}</h1>
+    <div className={twMerge('min-h-screen shrink-0 snap-center px-4', color)}>
+      <h1 className="block pb-2 pt-8 text-2xl font-bold">{title}</h1>
+      {infos &&
+        infos.map((info) => (
+          <div className="flex gap-2">
+            <Info width={20} height={20} className="shrink-0" />
+            <p className="grow text-sm">{info}</p>
+          </div>
+        ))}
       {children}
     </div>
   )
